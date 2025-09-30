0139139bd9a8414834e3aea85615cdccf4335dcf
MD NADIM <Nadim06787@gmail.com>
2025-09-30 05:45:06 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "nadim";
+//let score = "25"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false => 0
+
+let isLoggedIn = "nadim";
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(bolleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "nadim" => ture
+
+let someNumber = 33;
+
+let stringnumber = String(someNumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
