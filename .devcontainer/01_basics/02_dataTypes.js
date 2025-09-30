c5597294bb1967fd81009a663573a88cc8e5bf4a
MD NADIM <Nadim06787@gmail.com>
2025-09-30 05:23:11 +0000

    datatype

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer js/version
+
+// alert("3+3") // we are using nodejs, not browser
+
+// console.log(3 + 3); console.log("nadim"); // code redibility is high avoid unusable code
+
+let name = "nadim"
+let age = 25
+let isLoggedIn = false
+let state = null;
+
+
+// number => 2 to power 53
+// bigint 
+// string = "", ''
+// bollean = true/false
+// null => standlone value
+// indefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof state);     // object
+
